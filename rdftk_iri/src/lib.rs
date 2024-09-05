ff65a51e95e49ba0fdd2c2667d88927325aa06dd
Simon Johnston <johnstonskj@gmail.com>
2024-09-05 10:05:25 -0700

    style: add more to library warning config

--- rdftk_iri/src/lib.rs
+++ rdftk_iri/src/lib.rs
@@ -50,23 +50,42 @@ assert_eq!(
 */
 
 #![warn(
+    unknown_lints,
     // ---------- Stylistic
-    future_incompatible,
-    nonstandard_style,
+    absolute_paths_not_starting_with_crate,
+    elided_lifetimes_in_paths,
+    explicit_outlives_requirements,
+    macro_use_extern_crate,
+    nonstandard_style, /* group */
+    noop_method_call,
     rust_2018_idioms,
+    single_use_lifetimes,
     trivial_casts,
     trivial_numeric_casts,
+    // ---------- Future
+    future_incompatible, /* group */
+    rust_2021_compatibility, /* group */
     // ---------- Public
     missing_debug_implementations,
-    missing_docs,
+    // missing_docs,
     unreachable_pub,
     // ---------- Unsafe
     unsafe_code,
+    unsafe_op_in_unsafe_fn,
     // ---------- Unused
-    unused_extern_crates,
-    unused_import_braces,
-    unused_qualifications,
-    unused_results,
+    unused, /* group */
+)]
+#![deny(
+    // ---------- Public
+    exported_private_dependencies,
+    // ---------- Deprecated
+    anonymous_parameters,
+    bare_trait_objects,
+    ellipsis_inclusive_range_patterns,
+    // ---------- Unsafe
+    deref_nullptr,
+    drop_bounds,
+    dyn_drop,
 )]
 
 // ------------------------------------------------------------------------------------------------
