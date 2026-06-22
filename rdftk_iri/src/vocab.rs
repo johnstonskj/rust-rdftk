0078f9768d05febfe3041c5665b4759e7b05b91f
Simon Johnston <johnstonskj@gmail.com>
2026-06-22 15:13:50 -0700

    docs: finished vocabulary rustdoc

--- rdftk_iri/src/vocab.rs
+++ rdftk_iri/src/vocab.rs
@@ -18,8 +18,8 @@ use serde::{Deserialize, Serialize};
 /// common prefix, the namespace IRI, and an optional description string.
 ///
 /// These values are stored in *raw* form as character slices, the methods
-/// [`Vocabulary::prefix_as_name`] and [`Vocabulary::iri_as_iri`] will convert directly
-/// to appropriate types for use in the [`IriPrefixMap`] structure.
+/// [`Vocabulary::prefix_as_namespace`] and [`Vocabulary::iri_as_iri`] will convert directly
+/// to appropriate types for use in the [`IriPrefixMap`](../map/struct.IriPrefixMap.html) structure.
 ///
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
@@ -108,30 +108,87 @@ pub const VOCABULARY_DOAP: Vocabulary = Vocabulary::new("doap", "http://usefulin
 pub const VOCABULARY_FOAF: Vocabulary = Vocabulary::new("foaf", "http://xmlns.com/foaf/0.1/")
     .with_description("FOAF (an acronym of friend of a friend) is a machine-readable ontology describing persons, their activities and their relations to other people and objects. See [Wikipedia](https://en.wikipedia.org/wiki/FOAF).");
 
+///
+/// The GeoNames geographical database covers all countries and contains over eleven million placenames that
+/// are available for download free of charge.
+///
+/// ## Details
+///
+/// * **prefix** -- "geonames"
+/// * **IRI** -- `<http://www.geonames.org/ontology#>`
+/// * **See Also** -- [geonames.org](http://sws.geonames.org/)
+///
 pub const VOCABULARY_GEO_NAMES: Vocabulary =
     Vocabulary::new("geonames", "http://www.geonames.org/ontology#")
         .with_description("See <http://sws.geonames.org/>");
 
+///
+/// A Mapping between the ISO standard thesaurus model and SKOS.
+///
+/// ## Details
+///
+/// * **prefix** -- "isothes"
+/// * **IRI** -- `<http://purl.org/iso25964/skos-thes#>`
+/// * **See Also** -- [Mapping ISO 25964 data model and the SKOS schema](https://www.dublincore.org/specifications/skos-thes/ns/)
+///
 pub const VOCABULARY_ISO_SKOS: Vocabulary =
     Vocabulary::new("isothes", "http://purl.org/iso25964/skos-thes#");
 
+///
+/// This standard supports representing and querying geospatial data on the Semantic Web. GeoSPARQL
+/// defines a vocabulary for representing geospatial data in RDF, and it defines an extension to the
+/// SPARQL query language for processing geospatial data.
+///
+/// ## Details
+///
+/// * **prefix** -- "opengis"
+/// * **IRI** -- `<http://www.opengis.net/ont/geosparql#>`
+/// * **See Also** -- [GeoSPARQL – A Geographic Query Language for RDF Data](https://www.ogc.org/standards/geosparql/)
+///
 pub const VOCABULARY_OPEN_GIS: Vocabulary =
     Vocabulary::new("opengis", "http://www.opengis.net/ont/geosparql#")
         .with_description("See <https://www.ogc.org/standards/geosparql/>");
 
+///
+/// This is  a core ontology for organizational structures, aimed at supporting linked data publishing of
+/// organizational information across a number of domains. It is designed to allow domain-specific extensions
+/// to add classification of organizations and roles, as well as extensions to support neighbouring information
+/// such as organizational activities.
+///
+/// ## Details
+///
+/// * **prefix** -- "org"
+/// * **IRI** -- `<http://www.w3.org/ns/org#>`
+/// * **See Also** -- [The Organization Ontology](https://www.w3.org/TR/vocab-org/) and
+///   [Registered Organization Vocabulary](https://www.w3.org/TR/vocab-regorg/).
+///
 pub const VOCABULARY_ORG: Vocabulary = Vocabulary::new("org", "http://www.w3.org/ns/org#")
     .with_description(
         "See <https://www.w3.org/TR/vocab-org/> and <https://www.w3.org/TR/vocab-regorg/>",
     );
 
+///
+/// The OWL 2 Web Ontology Language, informally OWL 2, is an ontology language for the Semantic Web with
+/// formally defined meaning. OWL 2 ontologies provide classes, properties, individuals, and data values
+/// and are stored as Semantic Web documents.
+///
+/// ## Details
+///
+/// * **prefix** -- "owl"
+/// * **IRI** -- `<http://www.w3.org/2002/07/owl#>`
+/// * **See Also** -- [OWL 2 Web Ontology Language Document Overview (Second Edition)](https://www.w3.org/TR/owl2-overview/)
+///
 pub const VOCABULARY_OWL: Vocabulary = Vocabulary::new("owl", "http://www.w3.org/2002/07/owl#");
 
+///
+/// The core RDF language components.
 ///
 /// ## Details
 ///
 /// * **prefix** -- "rdf"
 /// * **IRI** -- `<http://www.w3.org/1999/02/22-rdf-syntax-ns#>`
 /// * **See Also** -- [w3.org](https://www.w3.org/TR/rdf11-concepts/)
+///
 pub const VOCABULARY_RDF: Vocabulary =
     Vocabulary::new("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#")
     .with_description("The Resource Description Framework (RDF) is a framework for representing information in the Web. See [w3.org](https://www.w3.org/TR/rdf11-concepts/)");
@@ -150,6 +207,17 @@ pub const VOCABULARY_RDF_SCHEMA: Vocabulary =
     Vocabulary::new("rdfs", "http://www.w3.org/2000/01/rdf-schema#")
         .with_description("RDF Schema provides a data-modelling vocabulary for RDF data; it is an extension of the basic RDF vocabulary. See [w3.org](https://www.w3.org/TR/rdf11-schema/).");
 
+///
+/// SKOS is an area of work developing specifications and standards to support the use of knowledge
+/// organization systems (KOS) such as thesauri, classification schemes, subject heading lists and
+/// taxonomies within the framework of the Semantic Web.
+///
+/// ## Details
+///
+/// * **prefix** -- "skos"
+/// * **IRI** -- `<http://www.w3.org/2004/02/skos/core#>`
+/// * **See Also** -- [SKOS Simple Knowledge Organization System - Home Page](https://www.w3.org/2004/02/skos/)
+///
 pub const VOCABULARY_SIOC: Vocabulary = Vocabulary::new("sioc", "http://rdfs.org/sioc/ns#")
     .with_description("See <https://www.w3.org/wiki/SIOC>");
 
@@ -169,6 +237,17 @@ pub const VOCABULARY_SKOS: Vocabulary =
             "The Simple Knowledge Organization System (SKOS) is a common data model for sharing and linking knowledge organization systems via the Web."
         );
 
+///
+/// SKOS-XL defines an extension for the Simple Knowledge Organization System, providing additional support
+/// for describing and linking lexical entities.
+///
+/// ## Details
+///
+/// * **prefix** -- "skosxl"
+/// * **IRI** -- `<http://www.w3.org/2008/05/skos-xl#>`
+/// * **See Also** -- [SKOS Simple Knowledge Organization System eXtension for Labels (SKOS-XL) Namespace
+///   Document - HTML Variant](https://www.w3.org/TR/skos-reference/skos-xl.html)
+///
 pub const VOCABULARY_SKOS_XL: Vocabulary =
     Vocabulary::new("skosxl", "http://www.w3.org/2008/05/skos-xl#");
 
@@ -177,9 +256,8 @@ pub const VOCABULARY_SKOS_XL: Vocabulary =
 ///
 /// ## Details
 ///
-/// * **prefix** -- ""
-/// * **IRI** -- `<>`
-/// * **See Also** -- []()
+/// * **prefix** -- "vann"
+/// * **IRI** -- `<http://purl.org/vocab/vann/>`
 ///
 pub const VOCABULARY_VANN: Vocabulary = Vocabulary::new("vann", "http://purl.org/vocab/vann/")
     .with_description(
@@ -192,9 +270,9 @@ pub const VOCABULARY_VANN: Vocabulary = Vocabulary::new("vann", "http://purl.org
 ///
 /// ## Details
 ///
-/// * **prefix** -- ""
-/// * **IRI** -- `<>`
-/// * **See Also** -- []()
+/// * **prefix** -- "void"
+/// * **IRI** -- `<http://rdfs.org/ns/void#>`
+/// * **See Also** -- [w3.org](https://www.w3.org/TR/void/)
 ///
 pub const VOCABULARY_VOID: Vocabulary = Vocabulary::new("void", "http://rdfs.org/ns/void#")
     .with_description(
@@ -206,24 +284,34 @@ pub const VOCABULARY_VOID: Vocabulary = Vocabulary::new("void", "http://rdfs.org
 ///
 /// ## Details
 ///
-/// * **prefix** -- ""
-/// * **IRI** -- `<>`
-/// * **See Also** -- []()
+/// * **prefix** -- "wot"
+/// * **IRI** -- `<http://xmlns.com/wot/0.1/>`
 ///
 pub const VOCABULARY_WOT: Vocabulary = Vocabulary::new("wot", "http://xmlns.com/wot/0.1/")
     .with_description("Store OpenPGP web-of-trust signatures in RDF documents.");
 
+///
+/// Standard vocabulary for XML, used mainly for the `xml:base` and `xml:lang` attributes.
+///
+/// ## Details
+///
+/// * **prefix** -- "xml"
+/// * **IRI** -- `<http://www.w3.org/XML/1998/namespace>`
+/// * **See Also** -- [Extensible Markup Language (XML) 1.0 (Fifth Edition)](https://www.w3.org/TR/xml/),
+///   [Namespaces in XML 1.0 (Third Edition)](https://www.w3.org/TR/xml-names/), and
+///   [XML Base (Second Edition)](https://www.w3.org/TR/xmlbase/).
+///
 pub const VOCABULARY_XML: Vocabulary =
     Vocabulary::new("xml", "http://www.w3.org/XML/1998/namespace#");
 
 ///
-/// Standard vocabulary for XML Schema, used mainly for datatypes.
+/// XML Schema's datatypes, used exten
 ///
 /// ## Details
 ///
-/// * **prefix** -- ""
-/// * **IRI** -- `<>`
-/// * **See Also** -- []()
+/// * **prefix** -- "xsd"
+/// * **IRI** -- `<http://www.w3.org/2001/XMLSchema#>`
+/// * **See Also** -- [W3C XML Schema Definition Language (XSD) 1.1 Part 2: Datatypes](https://www.w3.org/TR/xmlschema11-2/)
 ///
 pub const VOCABULARY_XML_SCHEMA: Vocabulary =
     Vocabulary::new("xsd", "http://www.w3.org/2001/XMLSchema#")
